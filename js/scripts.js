10216ea355cb645d3012e47bc658e39e485c2647
Nicole Freed <nicole.freed@gmail.com>
2017-06-14 09:54:03 -0700

    create document.ready, submit function, and CSS for play button

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// business logic goes here
+
+
+
+
+
+
+// user-interface logic goes here
+$(document).ready(function() {
+  $("button#play").click(function(event) {
+    // onclick, create new game with game board, players, etc.
+  });
+});
