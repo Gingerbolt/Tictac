e08c8f154699fadbd48f86c0e7ce59926e652207
Nicole Freed <nicole.freed@gmail.com>
2017-06-14 10:35:28 -0700

    add newGame function to reset board values between games; adjust corresponding spec

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ function Player(playerName, mark, first, playerTurn, wins) {
 };
 
 function Game(number, players, round, whoseTurn, gameOver, gameScore) {
-  this.number = number;  // global game number in sequence of games
+  // this.number = number;  // global game number in sequence of games
   this.players = players;  // 2-item array with each player's name
   this.round = round;  // automatically incremented after both players play
   this.whoseTurn = whoseTurn;  // game keeping track of whose turn it is
@@ -26,10 +26,24 @@ function Game(number, players, round, whoseTurn, gameOver, gameScore) {
   this.gameScore = gameScore;
 };
 
+function newGame() {
+  // reset game board and boxes to ""; create new players
+  $("input#text00").val("");
+  $("input#text01").val("");
+  $("input#text02").val("");
+  $("input#text10").val("");
+  $("input#text11").val("");
+  $("input#text12").val("");
+  $("input#text20").val("");
+  $("input#text21").val("");
+  $("input#text22").val("");
+};
+
 
 // user-interface logic goes here
 $(document).ready(function() {
   $("button#play").click(function(event) {
     // onclick, create new game with game board, players, etc.
+    newGame();
   });
 });
