ccc2273ad9d8f6ae1e2dd2a3d55200a18676285c
Nicole Freed <nicole.freed@gmail.com>
2017-06-14 11:33:03 -0700

    add functions to grab input from player fields, reset player fields

--- js/scripts.js
+++ js/scripts.js
@@ -9,24 +9,24 @@ function Board(idx, fullrow) {
   this.fullrow = fullrow; // Boolean
 };
 
-function Player(playerName, mark, first, playerTurn, wins) {
+function Player(playerName, playerNum, mark, first, wins) {
   this.playerName = playerName;
+  this.playerNum = playerNum;
   this.mark = mark;
   this.first = first;
-  this.playerTurn = playerTurn;  // Boolean; player keeping track of whether it's their turn or not
   this.wins = wins;
 };
 
-function Game(number, players, round, whoseTurn, gameOver, gameScore) {
+function Game(number, players, round, whoseTurn, gameOver, totalScore) {
   // this.number = number;  // global game number in sequence of games
-  this.players = players;  // 2-item array with each player's name
+  this.players = [];  // 2-item array with each player's name
   this.round = round;  // automatically incremented after both players play
   this.whoseTurn = whoseTurn;  // game keeping track of whose turn it is
   this.gameOver = gameOver;  // Boolean - yes or no
-  this.gameScore = gameScore;
+  this.totalScore = totalScore;  // numeric
 };
 
-function newGame() {
+function resetBoard() {
   // reset game board and boxes to ""; create new players
   $("input#text00").val("");
   $("input#text01").val("");
@@ -39,11 +39,43 @@ function newGame() {
   $("input#text22").val("");
 };
 
+function resetPlayers() {
+  $("input#player-1-name").val("");
+  $("input#player-2-name").val("");
+};
+
+  // newPlayer = new Player(playerName, mark, first, wins);
+//   var players = [];
+//   var nicole = new Player("Nicole", "X", true, 0);
+//   console.log(nicole);
+//   var jason = new Player("Jason", "0", false, 0);
+//   console.log(jason);
+//   newGame.players.forEach(function(playerName) {
+//     players.push(playerName);
+//   });
+//   console.log(newGame.players);
+// };
+
 
 // user-interface logic goes here
 $(document).ready(function() {
+  var Player1Name;
+  var Player2Name;
+  // var playerName;
+  // var newPlayer;
+  $("button#players").click(function(event) {
+    event.preventDefault();
+    Player1Name = $("input#player-1-name").val();
+    Player2Name = $("input#player-2-name").val();
+    console.log(Player1Name);
+    console.log(Player2Name);
+    resetPlayers();
+  });
+
   $("button#play").click(function(event) {
+    event.preventDefault();
     // onclick, create new game with game board, players, etc.
-    newGame();
+    // write text input fields prompting for players' names
+    resetBoard();
   });
 });
